14269ee9c2974fc38b6ed1b587b6955834dcf2a5
Jorge <jorge@pop-os.localdomain>
2021-09-15 14:56:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="main-container">
+        <img id="hero-image" src="https://res.cloudinary.com/acurunner79/image/upload/v1631730294/illustration-hero_wcngjy.svg" alt="hero"/>
+        <h2>Order Summary</h2>
+        <div id="description">
+          <h4 className="text-color-change">You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</h4>
+        </div>
+        <div id="mid-container">
+          <img id="music-icon" src="https://res.cloudinary.com/acurunner79/image/upload/v1631730491/icon-music_unjcof.svg" alt="music-icon"/>
+          <div id="plan-content">
+            <h4>Annual Plan</h4>
+            <h4 className="text-color-change">$59.99/year</h4>
+          </div>
+          <h4><a target="_blank" rel="noreferrer" href="https://jorgesotocoder.com">Change</a></h4>
+        </div>
+        <div id="button-container">
+          <button className="button">Proceed to Payment</button><br/>
+          <button className="button">Cancel Order</button>
+        </div>
+      </div>
     </div>
   );
 }
